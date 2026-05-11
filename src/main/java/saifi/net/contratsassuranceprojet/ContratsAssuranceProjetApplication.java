5230e608521fce81f4fce7c106a000c34c37f897
mohamed-saifi <saifimsc@gmail.com>
2026-05-11 10:02:23 +0200

    first commmit

---
+++ src/main/java/saifi/net/contratsassuranceprojet/ContratsAssuranceProjetApplication.java
@@ -0,0 +1,13 @@
+package saifi.net.contratsassuranceprojet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContratsAssuranceProjetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContratsAssuranceProjetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/saifi/net/contratsassuranceprojet/ContratsAssuranceProjetApplicationTests.java
@@ -0,0 +1,13 @@
+package saifi.net.contratsassuranceprojet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContratsAssuranceProjetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
